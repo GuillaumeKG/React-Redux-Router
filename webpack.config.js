0e4b2b9da29c196566d8d3e6bb500bbdfb203141
unknown <you@example.com>
2018-02-25 18:29:55 +0100

    updates

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: [
+        path.join(__dirname, './index.js')
+    ],
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        },
+        {
+            test: /\.sass$/,
+            loaders: ["style-loader", "css-loader", "sass-loader"]
+        }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+    },
+    devServer: {
+        contentBase: __dirname,
+        historyApiFallback: true
+    }
+}
